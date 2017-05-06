e562a5eb1174a14bb9fe8a19e789f9499a40fa9f
Anwesh43 <anweshthecool0@gmail.com>
2017-05-06 20:47:40 +0530

    Adding Collapsible Component

---
+++ components/CollapsibleComponent.js
@@ -0,0 +1,14 @@
+import {Collapsible,CollapsibleItem} from 'react-materialize'
+import React,{Component} from 'react'
+export default class CollapsibleComponent extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+      const collapsibleComponents = this.props.collapsibles
+      const collapsibles = collapsibleComponents.map((collapsibleComponent)=>(<CollapsibleItem className="white-text blue-darken-2" expanded={true} header={collapsibleComponent.header} icon='whatshot'>{collapsibleComponent.text}</CollapsibleItem>))
+      return (<Collapsible className="blue darken-2 white-text">
+                {collapsibles}
+            </Collapsible>)
+    }
+}
