b217fd69643c0a9f1221fcaa578b218b926adca9
Anwesh43 <anweshthecool0@gmail.com>
2017-05-07 11:06:54 +0530

    adding links for partners

--- partners.js
+++ partners.js
@@ -8,7 +8,8 @@ class PartnersComponent extends Component {
         super(props)
     }
     render() {
-        const partnersArray = ['We are associated with lot of companies in form of exploration and technology used for mining.','For exploration of minerals, PK Ores Pvt. Ltd has tied up with the most leading organization like Geomin Group. With this venture we are exploring the mineral resources in the whole world. For the best technology used for mining, who else is better then Triveni earth movers (p) ltd. no one in India. We have tied of with them to encase the opportunity of best technology used for mining to have better production.']
+        const partnersHtml = <p>For exploration of minerals, PK Ores Pvt. Ltd has tied up with the most leading organization like <a href="http://www.geomin.com">Geomin Group</a>. With this venture we are exploring the mineral resources in the whole world. For the best technology used for mining, who else is better then <a href="http://thriveni.com/">Thriveni earth movers (p) ltd. </a>no one in India. We have tied of with them to encase the opportunity of best technology used for mining to have better production.</p>
+        const partnersArray = ['We are associated with lot of companies in form of exploration and technology used for mining.',partnersHtml]
         const partnersCollectionComponent = <CollectionComponent className="blue darken-4" collections={partnersArray}/>
         const collapsibleObject = [{header:'Our Partners',text:partnersCollectionComponent}]
         return (<div>
