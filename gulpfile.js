a18bc9b1fece60483c2bb7a174dd10627c110835
Anwesh43 <anweshthecool0@gmail.com>
2017-05-07 12:18:22 +0530

    compiling clientjs from es2015 to es5

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp')
 const browserify = require('browserify')
-const pages = ['index','aboutus','services','contactus','partners']
+const pages = ['index','aboutus','services','contactus','partners','clients']
 gulp.task('compileES6',()=>{
     pages.forEach((page)=>{
       browserify(`${page}.js`).transform('babelify',{presets:['es2015','react']}).bundle().pipe(require('fs').createWriteStream(`public/js/${page}.js`))
