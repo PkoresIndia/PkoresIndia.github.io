2a5eccd001e859550e434a3e437e2cd4636a116c
Anwesh43 <anweshthecool0@gmail.com>
2017-05-08 22:26:20 +0530

    adding components folder

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import React,{Component} from 'react'
 export default class NavBar extends Component {
     constructor(props) {
         super(props)
-        this.state = {navs:[{text:"Home",url:"index.html"},{text:"About Us",url:"aboutus.html"},{text:"Contact Us",url:"contactus.html"},{text:"Services",url:"services.html"},{text:"Our Partners",url:"partners.html"},{text:'Our Clients',url:'clients.html'},{text:'Gallery',url:'gallery.html'}]}
+        this.state = {navs:[{text:"Home",url:"index.html"},{text:"About Us",url:"aboutus.html"},{text:"Our Business",url:"our-business.html"},{text:"Our Partners",url:"partners.html"},{text:'Our Clients',url:'clients.html'},{text:'Gallery',url:'gallery.html'},{text:"Contact Us",url:"contactus.html"}]}
     }
     render() {
         const navComponents = this.state.navs.map((nav)=>(<NavItem href={nav.url}>{nav.text}</NavItem>))
